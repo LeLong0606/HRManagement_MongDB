dce9a0c65c22cb87b63d271d2f1db4c833da6eab
LONG <lelong.06062002@gmail.com>
2025-04-25 14:26:45 +0700

    them folder data va cau hinh lai program

---
+++ Data/HRMSetting.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace HRManagement.Data
+{
+    public class HRMSetting
+    {
+        public string ConnectionString { get; set; } = null!;
+        public string DatabaseName { get; set; } = null!;
+        public Collections CollectionName { get; set; } = null!;
+    }
+
+    public class Collections
+    {
+        public string Employees { get; set; } = null!;
+        public string Departments { get; set; } = null!;
+        public string Attendance { get; set; } = null!;
+        public string LeaveRecords { get; set; } = null!;
+        public string PerformanceReviews { get; set; } = null!;
+        public string Notifications { get; set; } = null!;
+        public string Projects { get; set; } = null!;
+        public string EmployeeAccounts { get; set; } = null!;
+        public string Contracts { get; set; } = null!;
+        public string Salaries { get; set; } = null!;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
+using HRManagement.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.Configure<HRMSetting>(
+    builder.Configuration.GetSection("HRManagementDatabase")
+);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
